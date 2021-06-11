32754fea4d1c5a6845d7f9d4caa9937c46c3bbc7
Kevin Vandel <kvandel@jahia.com>
2021-06-11 13:32:26 +0200

    PAAS-1601: Escape backslashes before writing to jexperience cfg file

--- assets/jahia/update-jcustomer-password-in-jahia.py
+++ assets/jahia/update-jcustomer-password-in-jahia.py
@@ -7,6 +7,11 @@ from binascii import a2b_base64
 file_path = sys.argv[1]
 new_password = a2b_base64(str.encode(sys.argv[2])).decode("utf-8")
 
+# We need to escape backslashes (with a backslash) before writing to the cfg file
+new_password = new_password.translate(str.maketrans(
+    {"\\": r"\\"},
+))
+
 with fileinput.FileInput(file_path, inplace=True, backup='.bak') as file:
     for line in file:
         if 'jexperience.jCustomerPassword' in line:
